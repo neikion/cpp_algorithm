60c6bc6ead68e32d9d66145bbd5854c0d53eb53d
neikion <neikion43@gmail.com>
2023-09-17 17:02:34 +0900

    Add MST

---
+++ MinimumSpanningTree.h
@@ -0,0 +1,44 @@
+#ifndef MinimumSpenningTree_H
+#define MinimumSpenningTree_H
+#ifndef vector_h
+#define vector_h
+#include<vector>
+#endif // !vector
+
+#ifndef iostream_h
+#define iostream_h
+#include<iostream>
+#endif // !iostream
+
+#ifndef list_h
+#define list_h
+#include<list>
+#endif
+
+#ifndef algorithm_h
+#define algorithm_h
+#include <algorithm>
+#endif // !algorithm_h
+
+namespace mst {
+	using namespace std;
+
+	class node {
+	public:
+		int value;
+		node()=default;
+		node(int);
+	};
+	class Line {
+	public:
+		node* node1;
+		node* node2;
+		int cost;
+	};
+	class MSTTree {
+	public:
+		void init();
+		list<Line> getMST(int,list<Line>);
+	};
+}
+#endif
