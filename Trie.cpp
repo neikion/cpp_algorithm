642a6d77fb3b1fecb21e3883cd83add73eb13f34
neikion <neikion43@gmail.com>
2023-10-27 02:49:12 +0900

    Create trie namespace

---
+++ Trie.cpp
@@ -0,0 +1,8 @@
+#include"Trie.h"
+#include <iostream>
+namespace trie {
+
+	void MainTest() {
+		
+	}
+}
---
+++ Trie.h
@@ -0,0 +1,21 @@
+#ifndef trie_h
+#define trie_h
+
+#ifndef list_h
+#define list_h
+#include<list>
+#endif
+
+#ifndef string_h
+#define string_h
+#include<string>
+#endif
+namespace trie {
+	using namespace std;
+	
+	class node {
+	};
+	
+	void MainTest();
+}
+#endif
